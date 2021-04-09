5600d06cb3b756722f2fca406735bcb59cf2d5a4
Jeffrey <jeffrey.rossiter@me.com>
2021-04-08 21:28:28 -0700

    fix formatting in migration skeleton

--- src/assets/migrations/skeleton.js
+++ src/assets/migrations/skeleton.js
@@ -1,15 +1,14 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-      <% attributes.forEach(function(attribute) { %>
-        await queryInterface.addColumn('<%= tableName %>', '<%= attribute.fieldName %>', {
-          type: Sequelize.<%= attribute.dataFunction ? `${attribute.dataFunction.toUpperCase()}(Sequelize.${attribute.dataType.toUpperCase()})` : attribute.dataValues ? `${attribute.dataType.toUpperCase()}(${attribute.dataValues})` : attribute.dataType.toUpperCase() %>
-        })<% }) %>
+  up: async (queryInterface, Sequelize) => {<% attributes.forEach(function(attribute) { %>
+    await queryInterface.addColumn('<%= tableName %>', '<%= attribute.fieldName %>', {
+      type: Sequelize.<%= attribute.dataFunction ? `${attribute.dataFunction.toUpperCase()}(Sequelize.${attribute.dataType.toUpperCase()})` : attribute.dataValues ? `${attribute.dataType.toUpperCase()}(${attribute.dataValues})` : attribute.dataType.toUpperCase() %>
+    })<% }) %>
   },
 
   down: async (queryInterface, Sequelize) => {
-    <% attributes.forEach(function(attribute) { %>
-      await queryInterface.removeColumn('<%= tableName %>', '<%= attribute.fieldName %>')<% }) %>
+    <% attributes.forEach(function(attribute) { %>await queryInterface.removeColumn('<%= tableName %>', '<%= attribute.fieldName %>')
+    <% }) %>
   }
 };
