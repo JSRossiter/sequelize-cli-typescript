9869999f763eb3efd5fb4be6343245705d91baae
Jeffrey <jeffrey.rossiter@me.com>
2021-04-08 21:24:21 -0700

    fix formatting in migration skeleton

--- src/assets/migrations/skeleton.js
+++ src/assets/migrations/skeleton.js
@@ -5,13 +5,11 @@ module.exports = {
       <% attributes.forEach(function(attribute) { %>
         await queryInterface.addColumn('<%= tableName %>', '<%= attribute.fieldName %>', {
           type: Sequelize.<%= attribute.dataFunction ? `${attribute.dataFunction.toUpperCase()}(Sequelize.${attribute.dataType.toUpperCase()})` : attribute.dataValues ? `${attribute.dataType.toUpperCase()}(${attribute.dataValues})` : attribute.dataType.toUpperCase() %>
-        }),
-      <% }) %>
+        })<% }) %>
   },
 
   down: async (queryInterface, Sequelize) => {
     <% attributes.forEach(function(attribute) { %>
-      await queryInterface.removeColumn('<%= tableName %>', '<%= attribute.fieldName %>')
-    <% }) %>
+      await queryInterface.removeColumn('<%= tableName %>', '<%= attribute.fieldName %>')<% }) %>
   }
 };
